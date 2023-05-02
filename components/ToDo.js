105f6e65ad82a2217f40fe2b35ec5c10139e6c39
fayded <kevin.d.fay@gmail.com>
2023-05-01 20:48:17 -0400

    todo part deux

--- components/ToDo.js
+++ components/ToDo.js
@@ -8,8 +8,11 @@ export default function ToDo() {
       <div className="container">
         <h1 className="text-7xl my-20 text-center col-span-12 uppercase tracking-widest">To Do in ATL</h1>
       </div>
+      <section className="container">
+        <h1 className='my-8 text-2xl'>Wellness &amp; Fitness</h1>
+      </section>
       <section className="container justify-center grid grid-cols-12 gap-16">
-        <section className="col-span-6">
+        <section className="col-span-12 md:col-span-6 mb-8">
           <Image
             src="/chastain.jpg"
             alt="Chastain Park"
@@ -21,7 +24,7 @@ export default function ToDo() {
           <p>Buckhead's premier park featuring a 2.5 mile walking trail with tennis and pickleball courts</p>
           <a href="https://www.buckhead.com/neighborhoods/chastain-park/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>         
         </section>
-        <section className="col-span-6">
+        <section className="col-span-12 md:col-span-6 mb-8">
           <Image
             src="/bobbyJones.jpg"
             alt="Bobby Jones Golf Course"
@@ -34,7 +37,7 @@ export default function ToDo() {
         </section>
       </section>
       <section className="container justify-center grid grid-cols-12 gap-16">
-        <section className="col-span-6">
+        <section className="col-span-12 md:col-span-6 mb-8">
           <Image
             src="/barrys.jpg"
             alt="Barrys bootcamp"
@@ -46,7 +49,7 @@ export default function ToDo() {
           <p>Located in Buckhead Village District, the original high intensity interval workout with a combination of running and weights</p>
           <a href="https://www.bodycentralpilates.com/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>         
         </section>
-        <section className="col-span-6">
+        <section className="col-span-12 md:col-span-6 mb-8">
           <Image
             src="/stRegisSpa.jpg"
             alt="St Regis Spa"
@@ -58,8 +61,11 @@ export default function ToDo() {
           <a href="https://www.buckhead.com/neighborhoods/chastain-park/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>
         </section>
       </section>
+      <section className="container">
+        <h1 className='my-8 text-2xl'>Shopping</h1>
+      </section>
       <section className="grid grid-cols-6 gap-16 items-center container">
-        <div className="col-span-4">
+        <div className="col-span-12 md:col-span-4 mb-8">
           <Image
             src="/buckheadVillage.jpg"
             alt="Buckhead Village"
@@ -67,26 +73,29 @@ export default function ToDo() {
             height={500}
           />
         </div>
-        <section className="col-span-2">
+        <section className="col-span-12 md:col-span-2 mb-8">
           <h1 className="text-xl mb-2">Buckhead Village</h1>
           <p className="mb-2">Outdoor shopping and dining destination featuring high-end retail and global cuisines</p>
           <a href="https://www.buckheadvillagedistrict.com/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>
         </section>
       </section>
       <section className="grid grid-cols-12 gap-16 items-center container">
-        <div className="col-span-6">
+        <div className="col-span-12 md:col-span-6 mb-8">
           <h1 className="text-xl mb-2">The Lenox Mall</h1>
           <p className="mb-2">3393 Peachtree Rd NE, Atlanta, GA 30326</p>
           <p className="mb-2">Located catty-corner to Phipps Plaza, shopping Mall with anchor stores of Neiman Marcus &amp; Bloomingdale's <a href="https://www.buckheadvillagedistrict.com/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a></p>
         </div>
-        <div className="col-span-6">
+        <div className="col-span-12 md:col-span-6 mb-8">
           <h1 className="text-xl mb-2">Phipps Plaza</h1>
           <p className="mb-2">3500 Peachtree Rd NE, Atlanta, GA 30326</p>
           <p className="mb-2">Located catty-corner to Phipps Plaza, shopping Mall with anchor stores of Neiman Marcus &amp; Bloomingdale's <a href="https://www.buckheadvillagedistrict.com/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a></p>
         </div>
       </section>
+      <section className="container">
+        <h1 className='mt-4 mb-6 text-2xl'>Landmarks</h1>
+      </section>
       <section className="container justify-center grid grid-cols-12 gap-16">
-        <section className="col-span-6">
+        <section className="col-span-12 md:col-span-6 mb-8">
           <Image
             src="/ponceCity.jpg"
             alt="Ponce City Market"
@@ -98,80 +107,43 @@ export default function ToDo() {
           <p>Nestled in the Old Fourth Ward along the "Atlanta BeltLine" Eastside Trail, a market with a food hall, shops, and famous rooftop views and entertainment</p>
           <a href="https://www.poncecitymarket.com/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>         
         </section>
-        <section className="col-span-6">
+        <section className="col-span-12 md:col-span-6 mb-8">
           <Image
-            src="/bobbyJones.jpg"
-            alt="Bobby Jones Golf Course"
+            src="/botanical.jpg"
+            alt="Botanical Gardens"
             width={608}
             height={405}
           />
-          <h1 className="text-xl mt-6 mb-2">Bobby Jones Golf Course</h1>
-          <p>Distinguished public golf course design featuring multiple tees and large double greens</p>
-          <a href="https://www.bobbyjonesgc.com/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>
+
+          <h1 className="text-xl mt-6 mb-2">Botanical Gardens</h1>
+          <p>An urban oasis in Midtown boasting thirty acres of display gardens and shady woodlands featuring extensive plant and flower collections</p>
+          <a href="https://atlantabg.org/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>         
         </section>
       </section>
-      <section className="grid grid-cols-6 gap-16 items-center container">
-        <div className="col-span-4">
-          <Image
-            src="/ponceCity.jpg"
-            alt="Ponce City Market"
-            width={500}
-            height={500}
-          />
-        </div>
-        <section className="col-span-2">
-          <h1 className="text-xl mb-2">Ponce City Market</h1>
-          <p className="mb-2">Outdoor shopping and dining destination featuring high-end retail and global cuisines</p>
-          <a href="https://www.buckheadvillagedistrict.com/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>
-        </section>
-      </section>
-      <section className="grid grid-cols-6 gap-16 items-center container">
-        <section className="col-span-2">
-          <h1 className="text-xl mb-2">High Museum</h1>
-          <p className="mb-2">From nineteenth-century sculpture to contemporary folk art, the seven themed collections include more than 18,000 works of art from around the world</p>
-          <a href="https://high.org/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>
-        </section>
-        <div className="col-span-4">
+      <section className="container justify-center grid grid-cols-12 gap-16">
+        <section className="col-span-12 md:col-span-6 mb-8">
           <Image
             src="/highMuseum.jpg"
             alt="High Museum"
             width={500}
             height={500}
           />
-        </div>
+          <h1 className="text-xl mt-6 mb-2">High Museum</h1>
+          <p className="mb-2">From nineteenth-century sculpture to contemporary folk art, the seven themed collections include more than 18,000 works of art from around the world</p>
+          <a href="https://high.org/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>
+        </section>
+        <section className="col-span-12 md:col-span-6 mb-8">
+          <Image
+            src="/history.jpg"
+            alt="Atlanta History Museum"
+            width={500}
+            height={500}
+          />
+          <h1 className="text-xl mt-6 mb-2">Atlanta History Museum</h1>
+          <p className="mb-2">Our museum atrium serves as a hub for all things Atlanta History Center. From here, you’ll explore award-winning exhibitions highlighting some of the Atlanta’s most important moments, people, and artifacts.</p>
+          <a href="https://high.org/" target="_blank" className="my-4 flex items-center">Visit &nbsp; <HiOutlineChevronRight /></a>
+        </section>
       </section>
-      {/* <section className="grid grid-cols-6 gap-16 items-top container">
-        <div className="col-span-2 mb-8">
-          <Image
-            src="/buckheadvillage.jpg"
-            alt="Buckhead Village District"
-            width={384}
-            height={250}
-          />
-          <h2 className="text-md mt-4 mb-2">Buckhead Village District</h2>
-          <p>Outdoor shopping and dining destination featuring high-end retail and global cuisines <a href="https://www.buckheadvillagedistrict.com/">Visit</a></p>
-        </div>
-        <div className="col-span-2 mb-8">
-          <Image
-            src="/lenox.jpg"
-            alt="Lenox Park Mall"
-            width={384}
-            height={250}
-          />
-          <h2 className="text-md mt-4 mb-2">Lenox Park Mall</h2>
-          <p>Located catty-corner to Phipps Plaza, shopping Mall with anchor stores of Neiman Marcus & Bloomingdale's <a href="https://www.simon.com/mall/lenox-square">Visit</a></p>
-        </div>
-        <div className="col-span-2 mb-8">
-          <Image
-            src="/phipps.jpg"
-            alt="Phipps Plaza"
-            width={384}
-            height={250}
-          />
-          <h2 className="text-md mt-4 mb-2">Phipps Plaza</h2>
-          <p>•	Located catty-corner to Phipps Plaza, shopping Mall with anchor stores of Saks Fifth Avenue & Nordstrom <a href="https://www.simon.com/mall/phipps-plaza">Visit</a></p>
-        </div>
-      </section> */}
     </section>
   )
 }
--- components/ToEat.js
+++ components/ToEat.js
@@ -7,11 +7,11 @@ export default function ToEat() {
     <section className="toEat">
       <section className="container justify-center items-center grid grid-cols-12">
         <h1 className="text-7xl my-20 text-center col-span-12 uppercase tracking-widest">Eat in ATL</h1>
-        <p className="mb-14 col-span-6">Guests are highly encouraged to secure hotel reservations as soon as possible to secure the 
+        <p className="mb-14 col-span-12 md:col-span-6">Guests are highly encouraged to secure hotel reservations as soon as possible to secure the 
     discounted room rate using the respective link.</p>
       </section>
       <section className="grid grid-cols-6 gap-16 items-top container">
-        <div className="col-span-2 mb-8">
+        <div className="col-span-2 md:col-span-2 mb-8">
           <Image
             src="/umi.jpg"
             alt="Umi"
@@ -21,7 +21,7 @@ export default function ToEat() {
           <h2 className="text-md mt-4 mb-2">Umi</h2>
           <p>Located in the St. Regis courtyard, a stylish haunt serving sushi &amp; other sophisticated Japanese fare in an airy, contemporary setting.</p>
         </div>
-        <div className="col-span-2 mb-8">
+        <div className="col-span-12 md:col-span-2 mb-8">
           <Image
             src="/stcecilias.jpg"
             alt="St Cecilia"
@@ -31,7 +31,17 @@ export default function ToEat() {
           <h2 className="text-md mt-4 mb-2">St. Cecilia</h2>
           <p>Coastal European food specializing in pasta and seafood dishes in a stunning dining room and gorgeous spacious bar.</p>
         </div>
-        <div className="col-span-2 mb-8">
+        <div className="col-span-12 md:col-span-2 mb-8">
+          <Image
+            src="/storicoVino.jpg"
+            alt="Storico Vino"
+            width={384}
+            height={250}
+          />
+          <h2 className="text-md mt-4 mb-2">Storico Vino</h2>
+          <p>Located in Buckhead Village District, a menu rooted in the northern coast of Italy featuring a flourish of traditional choices with a Venetian-style wine bar.</p>
+        </div>
+        <div className="col-span-12 md:col-span-6 mb-8">
           <Image
             src="/gardenroom.jpg"
             alt="Garden Room"
@@ -41,7 +51,7 @@ export default function ToEat() {
           <h2 className="text-md mt-4 mb-2">The Garden Room</h2>
           <p>Located in the St. Regis, a chic restaurant serving New American cuisine &amp; cocktails in a plant-filled space with a glass roof.</p>
         </div>
-        <div className="col-span-2 mb-8">
+        <div className="col-span-12 md:col-span-2 mb-8">
           <Image
             src="/superica.jpg"
             alt="Superica"
@@ -51,7 +61,7 @@ export default function ToEat() {
           <h2 className="text-md mt-4 mb-2">Superica</h2>
           <p>Tex-Mex cuisine amidst a casual Old West atmosphere.</p>
         </div>
-        <div className="col-span-2 mb-8">
+        <div className="col-span-12 md:col-span-2 mb-8">
           <Image
             src="/leBilboquet.jpg"
             alt="Le Bilboquet"
@@ -61,7 +71,7 @@ export default function ToEat() {
           <h2 className="text-md mt-4 mb-2">Le Bilboquet</h2>
           <p>Located in Buckhead Village District, classic French cooking in a vibrant setting with quintessential Parisian elements.</p>
         </div>
-        <div className="col-span-2 mb-8">
+        <div className="col-span-12 md:col-span-2 mb-8">
           <Image
             src="/atlas.jpg"
             alt="Atlas"
@@ -71,16 +81,6 @@ export default function ToEat() {
           <h2 className="text-md mt-4 mb-2">Atlas</h2>
           <p>Located in the St. Regis, a menu inspired by partnerships with local farms using the freshest and most seasonal produce in a sophisticated setting adorned with an extraordinary collection of 20th-century modern art.</p>
         </div>
-        {/* <div className="col-span-2 mb-8">
-          <Image
-            src="/storicoVino.jpg"
-            alt="Storico Vino"
-            width={384}
-            height={250}
-          />
-          <h2 className="text-md mt-4 mb-2">Storico Vino</h2>
-          <p>Located in Buckhead Village District, a menu rooted in the northern coast of Italy featuring a flourish of traditional choices with a Venetian-style wine bar.</p>
-        </div> */}
       </section>
     </section>
   )
