20c9e2324bfa3846c86e69addb2fcae166d3dd02
fayded <kevin.d.fay@gmail.com>
2023-05-09 00:42:54 -0400

    final changes

--- components/WeekendEvents.js
+++ components/WeekendEvents.js
@@ -5,20 +5,20 @@ export default function WeekendEvents() {
   return (
     <section className="weekendEvents">
       <section className="container justify-center items-center grid grid-cols-12">
-        <h1 className="text-7xl text-center col-span-12 uppercase tracking-widest">Weekend Events in ATL</h1>
+        <h1 className="text-7xl text-center col-span-12 uppercase tracking-widest my-20">Weekend Events in ATL</h1>
       </section>
       <section className="container">
-      <p className="mt-10 mb-4"><span className="mr-4">The Wedding Day</span>|<span className="mx-4">March 16th, 2024</span>|<span className="mx-4">Gowns &amp; Tuxes</span></p>
+      <p className="mt-10 mb-6"><span className="mr-4">The Wedding Day</span>|<span className="mx-4">March 16th, 2024</span>|<span className="mx-4">Gowns &amp; Tuxes</span></p>
       </section>
       <section className="grid grid-cols-12 gap-16 items-start container">
         <div className="col-span-12 md:col-span-6 mb-8">
           <Image
             src="/ceremony.jpg"
             alt="The Cathedral at Christ the King"
-            width={500}
-            height={370}
+            width={609}
+            height={662}
           />
-          <h1 className="text-xl mt-4">The Ceremony</h1>
+          <h1 className="text-xl mt-6 mb-2">The Ceremony</h1>
           <p className="pb-6">Cathedral of Christ the King
             <br />6 o&apos;clock PM
           </p>
@@ -27,16 +27,16 @@ export default function WeekendEvents() {
           <Image
             src="/st-regis-tall.jpg"
             alt="St Regis"
-            width={608}
-            height={406}
+            width={609}
+            height={662}
           />
-          <h1 className="text-xl mt-4">The Celebration</h1>
+          <h1 className="text-xl mt-6 mb-2">The Celebration</h1>
           <p>The St. Regis
           <br />Immediately Following The Ceremony</p>
         </div>
       </section>
       <section className="container">
-        <p className="mt-4 mb-10"><span className="mr-4">The Day Before</span>|<span className="mx-4">March 15th, 2024</span>|<span className="mx-4">Cocktail Attire</span></p>
+        <p className="mt-4 mb-6"><span className="mr-4">The Day Before</span>|<span className="mx-4">March 15th, 2024</span>|<span className="mx-4">Cocktail Attire</span></p>
       </section>
       <section className="container justify-center grid grid-cols-12 gap-16">
         <section className="col-span-12 md:col-span-4 mb-8">
@@ -50,7 +50,7 @@ export default function WeekendEvents() {
           <p className="pb-6">Cathedral of Christ the King
             <br />5 o&apos;clock PM
             <br />
-            <span className="text-xs">* Invite Only</span>
+            <span className="text-xs">* Invite only</span>
           </p>
         </section>
         <section className="col-span-12 md:col-span-4 mb-8">
@@ -64,7 +64,7 @@ export default function WeekendEvents() {
           <p className="pb-6">Le Colonial
             <br />6 o&apos;clock PM
             <br />
-            <span className="text-xs">* Invite Only</span>
+            <span className="text-xs">* Invite only</span>
           </p>
         </section>
         <section className="col-span-12 md:col-span-4 mb-8">
