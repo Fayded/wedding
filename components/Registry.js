1730f43b6b6eb2572777c3cc41c3fd7ed4431b3e
Kevin Fay <131882997+faytalitrix@users.noreply.github.com>
2023-05-09 18:30:37 -0400

    coming soon...

--- components/Registry.js
+++ components/Registry.js
@@ -2,9 +2,10 @@ import React from 'react'
 
 export default function Registry() {
   return (
-    <section>
+    <section className="justify-center items-center grid">
       <section className="container justify-center items-center grid grid-cols-12">
-        <h1 className="text-7xl my-20 text-center col-span-12 uppercase tracking-widest">Registry Coming Soon.</h1>
+        <h1 className="text-7xl mt-10 mb-5 text-center col-span-12 uppercase tracking-widest">Registry</h1>
+        <p className="mb-14 col-span-8 col-start-3 text-center">Coming Soon.</p>
       </section>
     </section>  
   )
