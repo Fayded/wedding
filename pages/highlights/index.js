414f401622489994028dc3063ffc77d824edadbc
Fayded <kevin.d.fay@gmail.com>
2024-05-29 23:42:16 -0400

    video

--- pages/highlights/index.js
+++ pages/highlights/index.js
@@ -1,11 +1,21 @@
+import { useEffect, useRef } from "react";
 import RootLayout from "@/components/Layout";
 import HighlightsLayout from "@/components/HighlightsLayout";
 
-export default function Highlights() {
+export default function Highlights() {  
   return (
-    <>
-      <h1>Highlights</h1>
-    </>
+    <section className="flex justify-center mt-10">
+      <video controls preload="none" autoPlay width="100%" muted>
+        <source src="https://fayco.imgix.net/E-K_1080.mp4" type="video/mp4" />
+        <track
+          src="/path/to/captions.vtt"
+          kind="subtitles"
+          srcLang="en"
+          label="English"
+        />
+        Your browser does not support the video tag.
+      </video>
+    </section>
   );
 }
 
