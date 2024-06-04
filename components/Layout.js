8f3a9786f5fd5a39e06b2e290a9084efda23a344
Fayded <kevin.d.fay@gmail.com>
2024-06-03 21:48:29 -0400

    add hamburger

--- components/Layout.js
+++ components/Layout.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import Link from 'next/link';
 import NavBar from './NavBar';
-import { HiX } from "react-icons/hi";
+import { HiX, HiOutlineMenu } from "react-icons/hi";
 
 const RootLayout = ({ children }) => {
 	const [isOpen, setIsOpen] = useState(false);
@@ -16,10 +16,18 @@ const RootLayout = ({ children }) => {
 
   return (
     <>
-			<section className="container justify-start lg:justify-center items-center mt-10 mb-5 lg:mb-0 flex">
+			<section className="container justify-between lg:justify-center items-center mt-10 mb-5 lg:mb-0 flex">
 					<h1 className="text-2xl md:text-7xl md:mt-20 md:mb-5 text-center col-span-12 uppercase tracking-widest">
 						<Link href="/highlights" className="hover:no-underline">Highlights</Link></h1>
-					<button className={isOpen ? "lg:hidden p-4 md:mt-20 md:mb-5 transition-transform ease-in-out duration-300 text-xl md:text-4xl lg:hidden" : "lg:hidden p-4 rotate-45 transition-transform ease-in-out delay-150 text-xl md:mt-20 md:mb-5 md:text-4xl"} onClick={handleClick}><HiX /></button>
+						{isOpen ? (
+							<button className="lg:hidden p-4 text-xl md:mt-20 md:mb-5 md:text-4xl" onClick={handleClick}>
+								<HiX />
+							</button>
+						) : (
+							<button className="lg:hidden p-4 md:mt-20 md:mb-5 text-xl md:text-4xl" onClick={handleClick}>
+								<HiOutlineMenu />
+							</button>
+						)}
 			</section>
 			<p className="container text-sm md:text-base mb-10 md:mb-10 col-span-12 md:col-span-12 md:text-center gmb-2">
         Click on photo to download the high resolution version
