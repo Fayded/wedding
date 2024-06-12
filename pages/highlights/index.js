1c3f55749a7e7e07a291ae876e018b157a6e4b94
Fayded <kevin.d.fay@gmail.com>
2024-06-11 23:49:00 -0400

    video

--- pages/highlights/index.js
+++ pages/highlights/index.js
@@ -1,14 +1,25 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState} from "react";
 import RootLayout from "@/components/Layout";
 import HighlightsLayout from "@/components/HighlightsLayout";
 
 export default function Highlights() {
+	const videoRef = useRef();
+	const [muted, setMuted] = useState(false);
+	const [play, setPlay] = useState(false);
+
 	useEffect(() => {
-		const video = document.querySelector("video").scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" });
-	}, []);
+		if (videoRef.current) {
+			window.requestAnimationFrame(() => 
+				videoRef.current?.scrollIntoView({ behavior: "smooth", block: "end", inline: "nearest" })
+			)
+		}
+		videoRef.current.play();
+		videoRef.current.muted = false;
+	}, [])
+
   return (
     <section className="flex justify-center mt-10">
-      <video controls preload="none" autoPlay width="100%">
+      <video controls ref={videoRef} preload="none" muted width="100%" poster="https://fayco.imgix.net/Reception/1S5A3399.jpg">
 				<source src="https://fayco.imgix.net/E-K_432.mp4.mp4" media="(max-width: 599px)" type="video/mp4" />
         <source src="https://fayco.imgix.net/E-K_1080.mp4.mp4" type="video/mp4" />
         <track
