7670c1b391fb743fe248c6ec00bc15c1beca2fcc
Fayded <kevin.d.fay@gmail.com>
2024-06-11 21:41:00 -0400

    audio up

--- pages/highlights/index.js
+++ pages/highlights/index.js
@@ -8,7 +8,8 @@ export default function Highlights() {
 	}, []);
   return (
     <section className="flex justify-center mt-10">
-      <video controls preload="none" autoPlay width="100%" muted>
+      <video controls preload="none" autoPlay width="100%">
+				<source src="https://fayco.imgix.net/E-K_432.mp4.mp4" media="(max-width: 599px)" type="video/mp4" />
         <source src="https://fayco.imgix.net/E-K_1080.mp4.mp4" type="video/mp4" />
         <track
           src="/path/to/captions.vtt"
